2680d65a6cd94cb5ceada6da7782c5289d9ba0cf
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2026-07-31 11:47:46 +0900

    clear public

--- public/tilia/plugins/x-gpx-export/helpers.js
+++
@@ -1,6 +0,0 @@
-export function makeFileName(name) {
-  if (!name) {
-    return "track.gpx";
-  }
-  return name.toLowerCase().endsWith(".gpx") ? name : `${name}.gpx`;
-}
--- public/tilia/src/index.js
+++
@@ -1,4 +0,0 @@
-// Keep the public API behind one module so internal file moves do not leak to users.
-export { createDefaultTiliaApp, createTiliaApp } from "./app.js";
-export { builtins } from "./builtins.js";
-export { createBaseMap } from "./map/base.js";
